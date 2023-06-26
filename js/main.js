85b7ac28dcde58ee8c31432a6cf1d59abecaeb0c
MirkalaParra <mirkala.p@gmail.com>
2023-06-25 23:57:23 -0300

    estilado en CSS responsive

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*Nuestras mejores aliadas: recuerden que antes de escribir en el
+archivo de CSS, lo mejor es ir realizando pruebas con las
+herramientas del navegador.
+● Revisar que todo el contenido esté alineado con flexbox, será
+más fácil para adaptar.
+● Recuerden ajustar tamaños y margins o paddings según sea
+necesario.
+● Para evitar que el contenido se “estire” en tamaño Desktop,
+pueden aplicar un width: 100% combinado con un max-width
+(“ancho máximo”) en pixeles, por ejemplo, max-width: 800px;
+Indicaciones específicas:
+● font-size para h1 en desktop: 48px
+● font-size para h2 en desktop: 32px
+Bonus track: les proponemos para este Proyecto 2 que busquen en la
+web “cómo hacer con CSS para modificar los estilos de un
+elemento al pasar el mouse sobre este”. Luego, lo aplicarán sobre
+los elementos del menú y el botón “Descargar mi CV”, para que al
+pasar el cursor del mouse sobre ellos cambie algo, por ejemplo el
+color de fondo. */
+
+
+
+//const titulo = document.querySelector("h1");
+//titulo.
+
+//const otraFoto = document.querySelector("#foto");
+//otraFoto.src = "./img/Foto2 Mirkala.jpg";
+
+/*Para modificar parrafo primera clase (cuando cambiaba el color)
+const parrafo = document.querySelector( '#ejemplo' );
+parrafo.addEventListener('click', function(e) {
+ e.target.classList.toggle('parrafo-modificado');
+})
+
+
+
+Parrafo modificado
+const parrafo = document.querySelector( '#ejemplo' );
+parrafo.addEventListener('click', function(e) {
+ e.target.classList.toggle('parrafo-modificado');
+})*/
