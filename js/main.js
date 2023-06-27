d513d6f749112af3d14fc54ce04f85fea8973bb9
MirkalaParra <mirkala.p@gmail.com>
2023-06-27 00:26:31 -0300

    primera prueba en js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
-/*Nuestras mejores aliadas: recuerden que antes de escribir en el
-archivo de CSS, lo mejor es ir realizando pruebas con las
-herramientas del navegador.
-● Revisar que todo el contenido esté alineado con flexbox, será
-más fácil para adaptar.
-● Recuerden ajustar tamaños y margins o paddings según sea
-necesario.
-● Para evitar que el contenido se “estire” en tamaño Desktop,
-pueden aplicar un width: 100% combinado con un max-width
-(“ancho máximo”) en pixeles, por ejemplo, max-width: 800px;
-Indicaciones específicas:
-● font-size para h1 en desktop: 48px
-● font-size para h2 en desktop: 32px
+/*
 Bonus track: les proponemos para este Proyecto 2 que busquen en la
 web “cómo hacer con CSS para modificar los estilos de un
 elemento al pasar el mouse sobre este”. Luego, lo aplicarán sobre
@@ -20,8 +8,29 @@ color de fondo. */
 
 
 
-//const titulo = document.querySelector("h1");
-//titulo.
+/*titulo.*/
+const titulo = document.querySelector("h1");
+parrafo.addEventListener('click', function(e) {
+ e.target.classList.toggle('');
+})
+
+//los li de la nav cambie de color
+const menuItem = document.querySelectorAll('.menu');
+menuItem.forEach(function(item) { 
+    item.addEventListener('click', function(e) {
+    const currentItem = document.querySelector('.active');
+    currentItem.classList.remove('active');
+    e.target.classList.add('active');
+    })
+});
+
+//Modificar parrafo
+    let parrafo = document.querySelector( '#contacto');
+    parrafo.addEventListener('click', function(e) {
+    e.target.classList.toggle('parrafo-modificado');
+   })
+
+ 
 
 //const otraFoto = document.querySelector("#foto");
 //otraFoto.src = "./img/Foto2 Mirkala.jpg";
@@ -39,3 +48,4 @@ const parrafo = document.querySelector( '#ejemplo' );
 parrafo.addEventListener('click', function(e) {
  e.target.classList.toggle('parrafo-modificado');
 })*/
+
