39225a6c1ef5c9e8dbf7ada170ba0d884741c877
MirkalaParra <mirkala.p@gmail.com>
2023-07-02 01:29:42 -0300

    proyecto con Java script

--- js/main.js
+++ js/main.js
@@ -1,51 +1,27 @@
-/*
-Bonus track: les proponemos para este Proyecto 2 que busquen en la
-web “cómo hacer con CSS para modificar los estilos de un
-elemento al pasar el mouse sobre este”. Luego, lo aplicarán sobre
-los elementos del menú y el botón “Descargar mi CV”, para que al
-pasar el cursor del mouse sobre ellos cambie algo, por ejemplo el
-color de fondo. */
 
-
-
-/*titulo.*/
-const titulo = document.querySelector("h1");
-parrafo.addEventListener('click', function(e) {
- e.target.classList.toggle('');
-})
-
-//los li de la nav cambie de color
-const menuItem = document.querySelectorAll('.menu');
-menuItem.forEach(function(item) { 
-    item.addEventListener('click', function(e) {
-    const currentItem = document.querySelector('.active');
-    currentItem.classList.remove('active');
-    e.target.classList.add('active');
-    })
+const menuItem = document.querySelectorAll(".menu-item a");
+menuItem.forEach(function(item) {
+  item.addEventListener("click", function(e) {
+    e.target.classList.toggle("clicked");
+  });
 });
 
-//Modificar parrafo
-    let parrafo = document.querySelector( '#contacto');
-    parrafo.addEventListener('click', function(e) {
-    e.target.classList.toggle('parrafo-modificado');
-   })
 
- 
-
-//const otraFoto = document.querySelector("#foto");
-//otraFoto.src = "./img/Foto2 Mirkala.jpg";
-
-/*Para modificar parrafo primera clase (cuando cambiaba el color)
-const parrafo = document.querySelector( '#ejemplo' );
-parrafo.addEventListener('click', function(e) {
- e.target.classList.toggle('parrafo-modificado');
-})
+const titulo = document.querySelector("h1");
+titulo.addEventListener("mouseover", function(e) {
+    e.target.classList.toggle('resaltado');
+});
 
 
+const hablemos = document.querySelector("h2");
+hablemos.addEventListener("mouseover", function(e) {
+    e.target.classList.toggle('hablemos');
+});
 
-Parrafo modificado
-const parrafo = document.querySelector( '#ejemplo' );
-parrafo.addEventListener('click', function(e) {
- e.target.classList.toggle('parrafo-modificado');
-})*/
+const modificado = document.querySelectorAll(".resaltar");
+modificado.forEach(function(item) {
+  item.addEventListener("mouseup", function(e) {
+    e.target.classList.toggle("parrafo_modificado");
+  });
+});
 
